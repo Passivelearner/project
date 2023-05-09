1eadd20b57bd7d8a77a696e6a928ebe0b69ca004
Zyrill Marcelino <zyrill247@gmail.com>
2023-05-10 03:08:15 +0800

    search

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -7,9 +7,9 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   serverUrl = 'http://127.0.0.1:8000';
   apiUrl = this.serverUrl + '/api' ;
-  userId: null | string =  localStorage.getItem('UserId');
-  token: null | string =  localStorage.getItem('Token');
-  userRole: null | string =  localStorage.getItem('UserRole');
+  public userId: null | string =  localStorage.getItem('UserId');
+  public token: null | string =  localStorage.getItem('Token');
+  public userRole: null | string =  localStorage.getItem('UserRole');
 
   public get headers(){
     return new HttpHeaders({
